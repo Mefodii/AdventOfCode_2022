38ee036f8b67f492094e5986b79c38b9b0c7c44b
Mefodii <mefodii04@gmail.com>
2022-12-06 07:53:51 +0200

    Day 6 - 100%

--- days/day_6.py
+++ days/day_6.py
@@ -1,7 +1,22 @@
+def is_unique(marker):
+    return len(marker) == len(set(marker))
+
+
+def find_non_repeating(line, step):
+    for i in range(len(line)-step):
+        marker = line[i:i+step]
+        if is_unique(marker):
+            return i + step
+
+    return -1
+
+
 ###############################################################################
 def run_a(input_data):
-    return ""
+    result = find_non_repeating(input_data[0], 4)
+    return result
 
 
 def run_b(input_data):
-    return ""
+    result = find_non_repeating(input_data[0], 14)
+    return result
