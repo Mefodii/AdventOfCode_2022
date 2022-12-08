f24546272425550847ae3434876828bd4354607e
Mefodii <mefodii04@gmail.com>
2022-12-08 12:57:34 +0200

    Day 8 - 100%

--- days/day_8.py
+++ days/day_8.py
@@ -1,7 +1,66 @@
+from __future__ import annotations
+import math
+
+from utils.classes.Matrix import Matrix, Cell
+
+
+class Tree(Cell):
+    def __init__(self, x, y, value):
+        super().__init__(x, y, value)
+        self.is_visible = False
+        self.distances = []
+        self.score = 0
+
+    def calculate_visibility(self, trees: Matrix) -> bool:
+        x = self.x
+        y = self.y
+        conditions = [trees.is_edge(x, y)] + [self.is_side_visible(side) for side in trees.get_sides(x, y)]
+        self.is_visible = any(conditions)
+
+        return self.is_visible
+
+    def is_side_visible(self, side: list[Tree]) -> bool:
+        return all(tree < self for tree in side)
+
+    def calculate_distance(self, trees: Matrix):
+        sides = trees.get_sides(self.x, self.y)
+        sides[2] = sides[2][::-1]
+        sides[3] = sides[3][::-1]
+
+        self.distances = [self.calculate_side_distance(side) for side in sides]
+        self.calculate_score()
+
+    def calculate_side_distance(self, side: list[Tree]) -> int:
+        return next((i + 1 for i in range(len(side)) if self.value <= side[i].value), len(side))
+
+    def calculate_score(self) -> int:
+        self.score = math.prod(self.distances)
+        return self.score
+
+    def __repr__(self):
+        return f"{super().__repr__()}, score: {self.score}, dists:{self.distances}"
+
+
+def calculate_visible(trees):
+    [item["cell"].calculate_visibility(trees) for item in trees]
+
+
+def calculate_distance(trees):
+    [item["cell"].calculate_distance(trees) for item in trees]
+
+
 ###############################################################################
 def run_a(input_data):
-    return ""
+    trees = Matrix.init_matrix(input_data, func=lambda x, y, cell: Tree(x, y, int(cell)))
+    calculate_visible(trees)
+    result = len(list(filter(lambda t: t["cell"].is_visible, trees)))
+
+    return result
 
 
 def run_b(input_data):
-    return ""
+    trees = Matrix.init_matrix(input_data, func=lambda x, y, cell: Tree(x, y, int(cell)))
+    calculate_distance(trees)
+    result = max([item["cell"].score for item in trees])
+
+    return result
--- utils/classes/Matrix.py
+++ utils/classes/Matrix.py
@@ -1,3 +1,36 @@
+from __future__ import annotations
+
+from typing import Callable
+
+
+class Cell:
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+    def __repr__(self):
+        return f'{self.x}-{self.y} value: {self.value}'
+
+    def __lt__(self, other):
+        return self.value.__lt__(other.value)
+
+    def __le__(self, other):
+        return self.value.__le__(other.value)
+
+    def __gt__(self, other):
+        return self.value.__gt__(other.value)
+
+    def __ge__(self, other):
+        return self.value.__ge__(other.value)
+
+    def __eq__(self, other):
+        return self.value.__eq__(other.value)
+
+    def __ne__(self, other):
+        return self.value.__ne__(other.value)
+
+
 class Adjacent:
     UP = "U"
     DOWN = "D"
@@ -20,13 +53,13 @@ class Adjacent:
 
 class Matrix:
 
-    def __init__(self, height, width, init_value=None):
+    def __init__(self, height: int, width: int, init_value=None):
         self.matrix = [Matrix.init_row(width, init_value) for _ in range(height)]
         self.height = height
         self.width = width
         self.init_value = init_value
 
-    def get_adjacent(self, x, y, diagonal=False):
+    def get_adjacent(self, x, y, diagonal=False) -> Adjacent:
         adjacent = [Adjacent(Adjacent.UP, x, y - 1, self.get_cell_or_none(x, y - 1)),
                     Adjacent(Adjacent.DOWN, x, y + 1, self.get_cell_or_none(x, y + 1)),
                     Adjacent(Adjacent.LEFT, x - 1, y, self.get_cell_or_none(x - 1, y)),
@@ -46,6 +79,27 @@ class Matrix:
     def get_column(self, x):
         return [row[x] for row in self.matrix]
 
+    def get_left(self, x, y):
+        return self.get_row(y)[:x]
+
+    def get_right(self, x, y):
+        return self.get_row(y)[x+1:]
+
+    def get_top(self, x, y):
+        return self.get_column(x)[:y]
+
+    def get_bottom(self, x, y):
+        return self.get_column(x)[y+1:]
+
+    def get_sides(self, x: int, y: int):
+        """Get in line all sides from current coordinates. Clockwise, starting from right side (R,B,L,T)"""
+        return [
+            self.get_right(x, y),
+            self.get_bottom(x, y),
+            self.get_left(x, y),
+            self.get_top(x, y),
+        ]
+
     def get_cell(self, x, y):
         return self.matrix[y][x]
 
@@ -60,9 +114,34 @@ class Matrix:
     def set_cell(self, x, y, value):
         self.matrix[y][x] = value
 
+    def is_edge(self, x, y):
+        return x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1
+
     def map_row(self, y, row_data):
         for i in range(len(row_data)):
             self.matrix[y][i] = row_data[i]
+
+    def __iter__(self):
+        self.current_x = 0
+        self.current_y = 0
+        return self
+
+    def __next__(self):
+        if self.current_x >= self.width:
+            self.current_x = 0
+            self.current_y += 1
+
+        if self.current_y >= self.height:
+            raise StopIteration
+
+        item = {
+            "cell": self.get_cell(self.current_x, self.current_y),
+            "x": self.current_x,
+            "y": self.current_y,
+        }
+        self.current_x += 1
+
+        return item
 
     def __repr__(self):
         result = ""
@@ -74,4 +153,13 @@ class Matrix:
 
     @staticmethod
     def init_row(size, init_value=None):
-        return [init_value for x in range(size)]
+        return [init_value for _ in range(size)]
+
+    @staticmethod
+    def init_matrix(data, func: Callable = lambda x, y, cell: cell) -> Matrix:
+        matrix = Matrix(len(data), len(data[0]))
+        for y, row in enumerate(data):
+            for x, cell in enumerate(row):
+                matrix.set_cell(x, y, value=func(x, y, cell))
+
+        return matrix
