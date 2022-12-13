42a83b4cce8e90144b684f61d12476662119d409
Mefodii <mefodii04@gmail.com>
2022-12-13 07:45:15 +0200

    Day 13 - Draft

--- days/day_13.py
+++ days/day_13.py
@@ -1,5 +1,20 @@
+from __future__ import annotations
+import ast
+
+
+def build_pairs(data):
+    pairs = []
+    for i in range(0, len(data), 3):
+        left = ast.literal_eval(data[i])
+        right = ast.literal_eval(data[i + 1])
+        pairs.append([left, right])
+    return pairs
+
+
 ###############################################################################
 def run_a(input_data):
+    pairs = build_pairs(input_data)
+    print(pairs)
     return ""
 
 
