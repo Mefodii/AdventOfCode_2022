6bd7c5882ee848dfd605f6209435443fe374a477
Mefodii <mefodii04@gmail.com>
2022-12-27 14:54:00 +0200

    Day 15 - 90% slow

--- days/day_15.py
+++ days/day_15.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Tuple
+
 from utils.Regex import findall_numbers
 from utils.classes.Matrix import Cell, MatrixType, Matrix
 
@@ -55,6 +57,19 @@ class Cave(Matrix):
         for sensor in self.sensors:
             sensor.mark_row(y)
 
+    def check(self):
+        for y in self.get_column_range():
+            print(y)
+            self.check_row(y)
+
+    def get_first_free(self) -> Tuple[int, int] | None:
+        for y in self.get_column_range():
+            for x in self.get_row_range():
+                if self.get_cell_or_none(x, y) is None:
+                    return x, y
+
+        return -1, -1
+
     def add_sensor(self, sensor: Sensor) -> None:
         self.sensors.append(sensor)
         self.set_cell(sensor.x, sensor.y, sensor)
@@ -76,10 +91,10 @@ def init_cave(data) -> Cave:
         y_list.append(coords[1])
         y_list.append(coords[3])
 
-    min_x = min(x_list)
-    min_y = min(y_list)
-    max_x = max(x_list)
-    max_y = max(y_list)
+    min_x = min(x_list) - 1000000
+    min_y = min(y_list) - 1000000
+    max_x = max(x_list) + 1000000
+    max_y = max(y_list) + 1000000
 
     cave = Cave(min_x, min_y, max_x, max_y)
     for coords in parsed_data:
@@ -107,4 +122,19 @@ def run_a(input_data, sample=False):
 
 
 def run_b(input_data, sample=False):
-    return ""
+    cave = init_cave(input_data)
+
+    cave.min_x = 0
+    cave.min_y = 0
+    if sample:
+        cave.max_x = 20
+        cave.max_y = 20
+    else:
+        cave.max_x = 4000000
+        cave.max_y = 4000000
+
+    cave.check()
+    x, y = cave.get_first_free()
+    result = 4000000 * x + y
+
+    return result
